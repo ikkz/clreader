6c19d24cf458733e34f39fae13e66141a16307a1
cildhdi <miles_yang@foxmail.com>
2019-02-01 15:32:26 +0800

    fix type argument

--- lib/platform/gbk_urlencode.dart
+++ lib/platform/gbk_urlencode.dart
@@ -3,5 +3,5 @@ import 'dart:async';
 import 'package:clreader/platform/channel.dart';
 
 Future<String> gbk_urlencode(String str) async {
-  return await channel.invokeMethod<String>('gbk_urlencode', str);
+  return await channel.invokeMethod('gbk_urlencode', str);
 }
